6dc9bcb016c831ba54ff73b47dce6919a2a30b70
AkashDeep000 <akashdeepdas970@gmail.com>
2025-03-07 19:06:57 +0530

    fix

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,8 @@
 import { ResetPasswordForm } from "@/components/auth/reset-password-form";
 import { GalleryVerticalEnd } from "lucide-react";
-import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 export default function ResetPasswordPage() {
-    const searchParams = useSearchParams();
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
