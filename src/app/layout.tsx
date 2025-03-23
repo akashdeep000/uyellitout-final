0d28b837c3fdf0ceedcd78cc06e6e2e7f3ba6e2f
AkashDeep000 <akashdeepdas970@gmail.com>
2025-03-24 02:16:05 +0530

    hero page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 import { ReactQueryProviders } from "@/providers/react-query";
 import type { Metadata } from "next";
-import { Quicksand } from "next/font/google";
+import { Cabin, Quicksand } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 };
 
 const dMSans = Quicksand({ subsets: ["latin"], variable: "--font-sans", weight: ["400", "700"] });
+const dMCabin = Cabin({ subsets: ["latin"], variable: "--font-cabin", weight: ["400", "700"] });
 
 export default function RootLayout({
   children,
@@ -19,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={dMSans.variable}>
+      <body className={cn(dMSans.variable, dMCabin.variable)}>
         <TooltipProvider>
           <main>
             <ReactQueryProviders>
