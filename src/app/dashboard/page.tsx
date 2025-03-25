107ccf10a7417a9b0576d8a7e6c77265438b9491
AkashDeep000 <akashdeepdas970@gmail.com>
2025-03-25 14:45:30 +0530

    dashboard bolg poster mobile

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -233,7 +233,7 @@ export default function Page() {
             </div>
           </div>
           <div className="aspect-square grid grid-rows-[1fr_auto] gap-4">
-            <div className="hidden sm:block bg-[url(/blog-thumb.svg)] bg-cover bg-top rounded-2xl">
+            <div className="bg-[url(/blog-thumb.svg)] bg-cover bg-top rounded-2xl">
               <div className="h-full py-[20%] flex flex-row items-end font-semibold w-[60%] lg:text-xl md:text-sm sm:text-2xl px-4">
                 <div className="space-y-4">
                   <p className="">How to deal with<br />anxiety?</p>
